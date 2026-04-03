b40f02aba483dd75b0815620fabc30e10b2ed7ae
Gigarun <contact@gigarun.net>
2026-04-03 09:32:27 +0400

    config: autoriser gigarun.local

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,12 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
+  server: {
+    host: '0.0.0.0',
+  },
+  vite: {
+    preview: {
+      allowedHosts: ['gigarun.local', 'localhost'],
+    },
+  },
 });
